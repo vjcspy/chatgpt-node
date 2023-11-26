38eca5b82c11fe7a7407565c703728e5f67f274d
vjcspy <mr.vjcspy@gmail.com>
2023-11-26 21:09:06 +0700

    chore

---
+++ ecosystem.prod.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'chatgpt-node',
+            script: './bin/server.js',
+            instances: '2',
+            instance_var: 'INSTANCE_ID',
+            exec_mode: 'cluster',
+        },
+    ],
+};
